b99e3f43afbca1be8b5dfc1b043889a859ad505d
Al Sutton <al@funkyandroid.com>
2010-05-29 10:49:09 +0000

    Update to make use of ListActivity

--- FunkyExpenses/src/com/funkyandroid/banking/android/AccountsActivity.java
+++ FunkyExpenses/src/com/funkyandroid/banking/android/AccountsActivity.java
@@ -1,10 +1,12 @@
 package com.funkyandroid.banking.android;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
@@ -12,9 +14,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.MenuItem.OnMenuItemClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ResourceCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 import com.flurry.android.FlurryAgent;
 import com.funkyandroid.banking.android.data.AccountManager;
@@ -23,17 +30,18 @@ import com.funkyandroid.banking.android.data.SettingsManager;
 import com.funkyandroid.banking.android.expenses.demo.R;
 import com.funkyandroid.banking.android.ui.keypad.KeypadFactory;
 import com.funkyandroid.banking.android.ui.keypad.KeypadHandler;
+import com.funkyandroid.banking.android.utils.BalanceFormatter;
 import com.funkyandroid.banking.android.utils.Crypto;
 import com.funkyandroid.banking.android.utils.MenuUtil;
 
-public class AccountsActivity extends Activity 
+public class AccountsActivity extends ListActivity 
 	implements KeypadHandler.OnOKListener {
 	
 	/**
 	 * The list adapter holding the accounts list.
 	 */
 	
-	private AccountsListAdapter adapter;
+	private MyListAdapter adapter;
 
 	/**
 	 * The handler for showing keypads.
@@ -69,10 +77,10 @@ public class AccountsActivity extends Activity
         db = (new DBHelper(this)).getWritableDatabase();        
         Cursor accounts = AccountManager.getAll(db);
         startManagingCursor(accounts);
-		adapter = new AccountsListAdapter(AccountsActivity.this, accounts);
+		adapter = new MyListAdapter(accounts);
 		
-        ListView list = (ListView)findViewById(R.id.informationList);
-        list.setAdapter(adapter);
+		setListAdapter(adapter);
+		ListView list = getListView();
         list.setOnItemClickListener(adapter);
         list.setOnItemLongClickListener(adapter);
 
@@ -240,6 +248,67 @@ public class AccountsActivity extends Activity
     	}
     }
     
+    /**
+     * The adapter showing the list of server statuses
+     */
+
+    public final class MyListAdapter extends ResourceCursorAdapter 
+    	implements OnItemClickListener, OnItemLongClickListener {
+
+    	/**
+    	 * Constructor.
+    	 */
+    	public MyListAdapter(final Cursor cursor) {
+    		super(AccountsActivity.this, R.layout.account_list_item, cursor);
+    	}
+    	
+    	/**
+    	 * Populate an account view with the data from a cursor.
+    	 */
+    	
+    	@Override
+    	public void bindView(final View view, final Context context, final Cursor cursor) {
+    		((TextView)view.findViewById(R.id.name)).setText(cursor.getString(1));
+
+    		final long balance = cursor.getLong(3);
+    		final TextView value = (TextView)view.findViewById(R.id.value);
+    		if			( balance < 0 ) {
+    			value.setTextColor(Color.rgb(0xc0, 0x00, 0x00));
+    		} else if	( balance > 0 ) {
+    			value.setTextColor(Color.rgb(0x00, 0xc0, 0x00));
+    		} else {
+    			value.setTextColor(Color.rgb(0xcf, 0xc0, 0x00));
+    		}
+    				
+    		final StringBuilder valueString = new StringBuilder(32);
+    		valueString.append("Balance : ");
+    		BalanceFormatter.format(valueString, balance, cursor.getString(4));
+    		valueString.append(' ');
+    		value.setText(valueString.toString());
+    	}
+    	
+    	/**
+    	 * Handle clicks by opening a browser window for the app.
+    	 */
+    	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    		Context context = parent.getContext();
+    		Intent viewIntent = new Intent(context, EntriesActivity.class);
+    		viewIntent.putExtra("com.funkyandroid.banking.account_id", ((int)id & 0xffff));
+    		context.startActivity(viewIntent);    				
+    	}
+
+    	/**
+    	 * A long click takes the user to the edit page.
+    	 */
+    	public boolean onItemLongClick(AdapterView<?> parent, View view, int position,
+    			long id) {
+    		Context context = parent.getContext();
+    		Intent viewIntent = new Intent(context, EditAccountActivity.class);
+    		viewIntent.putExtra("com.funkyandroid.banking.account_id", ((int)id & 0xffff));
+    		context.startActivity(viewIntent);    				
+    		return true;
+    	}
+    }
     
     /**
      * A post onCreate initialisation thread.

c7c81835d162e19eae8c08fe5241f236f278ef44
Al Sutton <al@funkyandroid.com>
2012-06-11 15:10:50 +0100

    Added long click listener

--- src/com/funkyandroid/banking/android/CategoriesReportFragment.java
+++ src/com/funkyandroid/banking/android/CategoriesReportFragment.java
@@ -9,23 +9,42 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.ResourceCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
 import com.funkyandroid.banking.android.data.Account;
 import com.funkyandroid.banking.android.data.AccountManager;
 import com.funkyandroid.banking.android.data.CategoryManager;
 import com.funkyandroid.banking.android.data.CurrencyManager;
+import com.funkyandroid.banking.android.data.DBHelper;
 import com.funkyandroid.banking.android.expenses.demo.R;
 import com.funkyandroid.banking.android.utils.BalanceFormatter;
+import com.funkyandroid.banking.android.utils.DatabaseRawQueryCursorLoader;
 
-public class CategoriesReportFragment extends ListFragment {
+public class CategoriesReportFragment extends ListFragment
+	implements LoaderManager.LoaderCallbacks<Cursor>  {
+
+	/**
+	 * The query to get the categories for an account
+	 */
+
+	private static final String ACCOUNT_CATS_QUERY =
+			"SELECT c._id as _id, c.name, sum(t.amount), count(*) FROM " +
+				DBHelper.CATEGORIES_TABLE_NAME + " c, " +
+				DBHelper.ENTRIES_TABLE_NAME + " t WHERE t.account_id = ? AND t.category_id = c._id GROUP BY t.category_id";
+
+	/**
+	 * The Adapter for the list
+	 */
+
+	private SpendingReportListAdapter mAdapter;
 
 	/**
 	 * The ID of the account being viewed.
@@ -56,7 +75,7 @@ public class CategoriesReportFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-    	int accountId = getArguments().getInt("com.funkyandroid.banking.account_id", -1);
+        int accountId = getArguments().getInt("com.funkyandroid.banking.account_id", -1);
     	if( accountId == -1 ) {
     		getActivity().finish();
     		return;
@@ -70,37 +89,32 @@ public class CategoriesReportFragment extends ListFragment {
     	}
 
     	currencySymbol = CurrencyManager.getSymbol(database, account.currency);
+
+    	mAdapter = new SpendingReportListAdapter();
+    	setListAdapter(mAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
     }
 
     /**
-     * Called whenever the activity becomes visible.
+     * Ensure the long click listener is set when we start
      */
-
     @Override
     public void onStart() {
     	super.onStart();
-    	updateBalance(AccountManager.getBalanceById(((DatabaseReadingActivity)getActivity()).getReadableDatabaseConnection(), account.id));
-    	((ResourceCursorAdapter)getListAdapter()).notifyDataSetChanged();
+
+        getListView().setOnItemLongClickListener(longClickListener);
     }
 
-    /**
-     * Creates the view for this fragment
-     */
+	/**
+	 * Restart the loader if we're resumed.
+	 */
 
-    @Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    	final View view = inflater.inflate(R.layout.categories, container, false);
-
-    	final SQLiteDatabase database = ((DatabaseReadingActivity)getActivity()).getReadableDatabaseConnection();
-        Cursor categoryCursor = CategoryManager.getForAccount(database, account.id);
-    	getActivity().startManagingCursor(categoryCursor);
-
-    	setListAdapter(new SpendingReportListAdapter(categoryCursor));
-    	ListView listView = (ListView)view.findViewById(android.R.id.list);
-    	listView.setOnItemLongClickListener(longClickListener);
-
-    	return view;
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+        getLoaderManager().restartLoader(0, null, this);
+	}
 
 	/**
 	 * Handle the clicking of a child object by taking the user
@@ -123,19 +137,6 @@ public class CategoriesReportFragment extends ListFragment {
 		view.getContext().startActivity(intent);
 		super.onListItemClick(parent, view, position, id);
 	}
-
-   	/**
-     * Update the current account balance
-     */
-
-    public void updateBalance(long newBalance) {
-    	StringBuilder balanceText = new StringBuilder(32);
-    	balanceText.append("Current balance : ");
-		BalanceFormatter.format(balanceText, newBalance, currencySymbol);
-
-    	TextView textView = (TextView) getView().findViewById(R.id.balance);
-    	textView.setText(balanceText.toString());
-    }
 
     /**
      * Handle a long press by showing the edit category dialog.
@@ -162,7 +163,6 @@ public class CategoriesReportFragment extends ListFragment {
 							return;
 						}
 						CategoryManager.updateCategory(database, categoryId, catName.getText().toString());
-						((ResourceCursorAdapter)CategoriesReportFragment.this.getListAdapter()).getCursor().requery();
 					}
     			})
     			.setNegativeButton(R.string.cancelButtonText, null)
@@ -173,6 +173,27 @@ public class CategoriesReportFragment extends ListFragment {
 
     }
 
+	@Override
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		final SQLiteDatabase db = ((DatabaseReadingActivity)getActivity()).getReadableDatabaseConnection();
+		return new DatabaseRawQueryCursorLoader(getActivity(), db, ACCOUNT_CATS_QUERY, new String[] {Integer.toString(account.id)});
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		mAdapter.swapCursor(data);
+        if (isResumed()) {
+            setListShown(true);
+        } else {
+            setListShownNoAnimation(true);
+        }
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		 mAdapter.swapCursor(null);
+	}
+
 	/**
 	 * The list adapter used for the expandable tree.
 	 */
@@ -180,8 +201,8 @@ public class CategoriesReportFragment extends ListFragment {
 	public class SpendingReportListAdapter
 		extends ResourceCursorAdapter {
 
-		public SpendingReportListAdapter(final Cursor cursor) {
-			super(CategoriesReportFragment.this.getActivity(), R.layout.category_list_item, cursor);
+		public SpendingReportListAdapter() {
+			super(getActivity(), R.layout.category_list_item, null, 0);
 		}
 
 		@Override
